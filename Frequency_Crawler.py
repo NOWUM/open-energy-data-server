453ec95f8f76d25ec785fb8b7f6a168d73a43427
Florian Maurer <maurer@fh-aachen.de>
2021-12-06 10:32:27 +0100

    update crawlers for postgresql

--- Frequency_Crawler.py
+++ Frequency_Crawler.py
@@ -7,6 +7,7 @@ import requests
 import io
 import zipfile
 
+
 def download_extract_zip(url):
     """
     Download a ZIP file and extract its contents in memory
@@ -18,13 +19,12 @@ def download_extract_zip(url):
             with thezip.open(zipinfo) as thefile:
                 yield zipinfo.filename, thefile, len(thezip.infolist())
 
-def crawl_frequency(db_conn=None, influx_conn=None):
-    for year in range(2011, 2020):
+
+def crawl_frequency(db_conn, first=2011, last=2020):
+    for year in range(first, last+1):
         print(year)
         url = f'https://www.50hertz.com/Portals/1/Dokumente/Transparenz/Regelenergie/Archiv%20Netzfrequenz/Netzfrequenz%20{year}.zip'
-        #response = requests.get(url)
-        # with open(f"frequency_{year}.zip", 'w') as f:
-        #    f.write(zipcontent)
+
         for name, thefile, count in download_extract_zip(url):
             print(name)
             if count == 1:  # only 2010
@@ -39,48 +39,36 @@ def crawl_frequency(db_conn=None, influx_conn=None):
                 del df['date_time']
             else:
                 df = pd.read_csv(thefile, sep=',', header=None,
-                                 parse_dates=[[0, 1]], infer_datetime_format=True
+                                 parse_dates=[[0, 1]],
+                                 infer_datetime_format=True
                                  )
                 if len(df.columns) == 3:
                     del df[2]
                 df.columns = ['date_time', 'frequency']
                 df.set_index('date_time')
-            if db_conn:
-                df.to_sql('frequency', db_conn, if_exists='append')
-            if influx_conn:
-                influx_conn.write_points('energies', 'network_frequency', df)#, tags = {})
-            #df.to_sql(str(year), conn2, if_exists='append')
+            df.to_sql('frequency', db_conn, if_exists='append')
 
 
 if __name__ == '__main__':
-    year = 2010
-    thefile = 'Netzfrequenz 2019/201901_Frequenz.csv'
-    thefile = 'Netzfrequenz 2011/201101_Frequenz.txt'
-    thefile = 'Netzfrequenz 2010/Frequenz2010.csv'
-    # try parsing 2010 csv files
     if False:
-        ddd = pd.read_csv(thefile, sep=';', decimal=",", header=None,
-                          names=['date_time', 'frequency'])
+        year = 2010
+        thefile = 'Netzfrequenz 2019/201901_Frequenz.csv'
+        thefile = 'Netzfrequenz 2011/201101_Frequenz.txt'
+        thefile = 'Netzfrequenz 2010/Frequenz2010.csv'
+        # try parsing 2010 csv files
+        conn = sqlite3.connect('freq.db')
+        crawl_frequency(conn)
 
-        #df.index = pd.to_datetime(df['date_time'], format='%d.%m.%Y %H:%M:%S')
-        #del df['date_time']
-        times = []
-        for i in range(3000):
-            st = time.time()
-            k = 10000
-            start = i*k
-            end = (i+1)*k
-            df = ddd[start:end]
-            df.index = pd.to_datetime(df['date_time'], infer_datetime_format=True)
-            times.append(time.time()-st)
+        import matplotlib.pyplot as plt
+        sql = 'select date_time, frequency from frequency where date_time>2019-01-01'
+        df = pd.read_sql(sql, conn)
+        plt.plot(df['date_time'], df['frequency'])
 
-    conn = sqlite3.connect('freq.db')
-    # influxClient = DataFrameClient(dbAddress, dbPort, dbUser, dbPassword, dbName)
-    crawl_frequency(conn) # influxClient)
-
-
-    import matplotlib.pyplot as plt
-    conn = sqlite3.connect('freq.db')
-    sql ='select date_time, frequency from frequency where date_time>2019-01-01'
-    df = pd.read_sql(sql, conn)
-    plt.plot(df['date_time'], df['frequency'])
+    from sqlalchemy import create_engine
+    import os
+    user=os.getenv('TIMESCALEDB_USER', 'opendata'),
+    password=os.getenv('TIMESCALEDB_PASSWORD', 'opendata'),
+    database=os.getenv('TIMESCALEDB_DATABASE', 'frequency'),
+    host=os.getenv('TIMESCALEDB_HOST', '10.13.10.41'),
+    port=int(os.getenv('TIMESCALEDB_PORT', 5432))
+    create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
--- OpenDWD_Crawler.py
+++ OpenDWD_Crawler.py
@@ -5,9 +5,9 @@ from shapely.geometry import shape, Point
 import shapefile
 import pandas as pd
 import numpy as np
-from sqlalchemy import create_engine
 import logging
 import os
+import sqlite3
 
 log = logging.getLogger('openDWD_cosmo')
 log.setLevel(logging.INFO)
@@ -15,8 +15,7 @@ log.setLevel(logging.INFO)
 
 class OpenDWDCrawler:
 
-    def __init__(self, init_plz_matrix=False, user='opendata', password='opendata',
-                 database='weather', host='10.13.10.41', port=5432):
+    def __init__(self, engine, init_plz_matrix=False, folder='./grb_files'):
         log.info('creating crawler')
         # base url and parameters to get the weather data from open dwd cosmo model
         self.base_url = 'https://opendata.dwd.de/climate_environment/REA/COSMO_REA6/hourly/2D/'
@@ -32,40 +31,44 @@ class OpenDWDCrawler:
                            clouds='CLCT/CLCT.2D.')
 
         # connection to open data platform fh aachen
-        self.engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
+        self.engine = engine
+        if type(self.engine) != sqlite3.Connection:
+            query = "CREATE TABLE IF NOT EXISTS public.cosmo( "\
+                    "timestamp timestamp without time zone NOT NULL, "\
+                    "plz double precision, "\
+                    "lat double precision, "\
+                    "lon double precision, "\
+                    "temperature_avg double precision, "\
+                    "temperature_min double precision, "\
+                    "temperature_max double precision, "\
+                    "radiation_dif double precision, "\
+                    "radiation_dir double precision, "\
+                    "wind_meridional double precision, "\
+                    "wind_zonal double precision, "\
+                    "rain_con double precision,"\
+                    "rain_gsp double precision, "\
+                    "clouds double precision, "\
+                    "PRIMARY KEY (timestamp , lat, lon));"
 
-        query = "CREATE TABLE IF NOT EXISTS public.cosmo( "\
-                "timestamp timestamp without time zone NOT NULL, "\
-                "plz double precision, "\
-                "lat double precision, "\
-                "lon double precision, "\
-                "temperature_avg double precision, "\
-                "temperature_min double precision, "\
-                "temperature_max double precision, "\
-                "radiation_dif double precision, "\
-                "radiation_dir double precision, "\
-                "wind_meridional double precision, "\
-                "wind_zonal double precision, "\
-                "rain_con double precision,"\
-                "rain_gsp double precision, "\
-                "clouds double precision, "\
-                "PRIMARY KEY (timestamp , lat, lon));"
+            self.engine.execute(query)
 
-        self.engine.execute(query)
+            query = "SELECT create_hypertable('cosmo', 'timestamp', create_default_indexes => FALSE, " \
+                "if_not_exists => TRUE, migrate_data => TRUE);"
 
-        query = "SELECT create_hypertable('cosmo', 'timestamp', create_default_indexes => FALSE, " \
-               "if_not_exists => TRUE, migrate_data => TRUE);"
-
-        self.engine.execute(query)
+            self.engine.execute(query)
 
         self.weather_file = 'weather.grb'
+        self.folder = folder
+
+        if not os.path.exists(self.folder):
+            os.makedirs(self.folder)
         # if no plz matrix is saved in ./crawler build an new one
         if init_plz_matrix:
-            shape_germany = shapefile.Reader(r'./crawler/plz_geo.shp')
+            shape_germany = shapefile.Reader(r'./plz_geo.shp')
             plz_areas = {feature.record['plz']: shape(feature.shape.__geo_interface__)
                          for feature in shape_germany.shapeRecords()}
-            lat_coordinates = np.load(r'./crawler/lat_coordinates.npy')
-            lon_coordinates = np.load(r'./crawler/lon_coordinates.npy')
+            lat_coordinates = np.load(r'./lat_coordinates.npy')
+            lon_coordinates = np.load(r'./lon_coordinates.npy')
             self.plz_matrix = np.zeros(lon_coordinates.shape)
             rows, cols = self.plz_matrix.shape
             for i in range(rows):
@@ -74,16 +77,17 @@ class OpenDWDCrawler:
                         if area.contains(Point((lon_coordinates[i][j], lat_coordinates[i][j]))):
                             self.plz_matrix[i][j] = int(key)
                             break
-            np.save(r'./crawler/plz_matrix', self.plz_matrix, allow_pickle=True)
+            np.save(r'./plz_matrix',
+                    self.plz_matrix, allow_pickle=True)
         else:
-            self.plz_matrix = np.load(r'./crawler/plz_matrix.npy')
+            self.plz_matrix = np.load(r'./plz_matrix.npy')
 
         log.info('crawler created')
 
     def __del__(self):
         for key in self.codecs.keys():
             try:
-                file_name = './grb_files/' + str(key) + '_' + self.weather_file
+                file_name = f'{self.folder}/{key}_{self.weather_file}'
                 os.remove(file_name)
             except:
                 log.exception('error cleaning up file')
@@ -91,19 +95,20 @@ class OpenDWDCrawler:
 
     def save_dwd_in_file(self, typ='temperature', year='1995', month='01'):
         # get weather parameter with given typ
-        url = self.base_url + f'{self.codecs[typ]}{year}{month}.grb.bz2'
+        url = f'{self.base_url}{self.codecs[typ]}{year}{month}.grb.bz2'
         response = requests.get(url)
-        log.info(f'get weather for {typ} with status code {response.status_code}')
+        log.info(
+            f'get weather for {typ} with status code {response.status_code}')
         # unzip an save data in file (parameter_weather.grb)
         weather_data = bz2.decompress(response.content)
-        file_name = './grb_files/' + str(typ) + '_' + self.weather_file
+        file_name = f'{self.folder}/{typ}_{self.weather_file}'
         with open(file_name, 'wb') as file:
             file.write(weather_data)
         log.info(f'file {file_name} saved')
 
     def read_grb_file(self, hour, counter, typ):
         # load dwd file with given typ
-        file_name = './grb_files/' + str(typ) + '_' + self.weather_file
+        file_name = f'{self.folder}/{typ}_{self.weather_file}'
         weather_data = pygrib.open(file_name)
         # extract the selector to get the correct parameter
         selector = str(weather_data.readline()).split('1:')[1].split(':')[0]
@@ -117,12 +122,14 @@ class OpenDWDCrawler:
         lat_values, lon_values = data_.latlons()
         df['lat'] = lat_values.reshape(-1, )[self.plz_matrix.reshape(-1,) > 0]
         df['lon'] = lon_values.reshape(-1, )[self.plz_matrix.reshape(-1,) > 0]
-        df['plz'] = self.plz_matrix.reshape(-1, )[self.plz_matrix.reshape(-1,) > 0]
+        df['plz'] = self.plz_matrix.reshape(-1,
+                                            )[self.plz_matrix.reshape(-1,) > 0]
         df['timestamp'] = hour
 
-        log.info(f'read data for typ: {typ} and hour: {counter} of {size} in month {hour.month}')
+        log.info(
+            f'read data for typ: {typ} and hour: {counter} of {size} in month {hour.month}')
         weather_data.close()
-        log.info(f'closed weather file')
+        log.info('closed weather file')
 
         return df
 
@@ -137,11 +144,12 @@ class OpenDWDCrawler:
                 # for each month get the dwd data for all given parameters in __init__
                 for parameter in self.codecs.keys():
                     month = f'{date.month:02d}'
-                    self.save_dwd_in_file(year=str(date.year), month=month, typ=parameter)
+                    self.save_dwd_in_file(
+                        year=str(date.year), month=month, typ=parameter)
 
                 # build dataframe and write data for each hour in month
                 hours = pd.date_range(start=pd.to_datetime(date), end=pd.to_datetime(date) + pd.DateOffset(months=1),
-                                    freq='h')[:-1]
+                                      freq='h')[:-1]
 
                 counter = 0                                                 # month hour counter
                 for hour in hours:
@@ -152,24 +160,28 @@ class OpenDWDCrawler:
                             df = self.read_grb_file(hour, counter, parameter)
                             init_df = False
                         else:
-                            df[parameter] = self.read_grb_file(hour, counter, parameter)[parameter]
+                            df[parameter] = self.read_grb_file(
+                                hour, counter, parameter)[parameter]
 
-                    log.info(f'build dataset for import')
+                    log.info('build dataset for import')
 
                     index = pd.MultiIndex.from_arrays([df['timestamp'], df['lat'], df['lon']],
-                                                    names=['timestamp', 'lat', 'lon'])
+                                                      names=['timestamp', 'lat', 'lon'])
                     df.index = index
                     del df['lon']
                     del df['lat']
                     del df['timestamp']
-                    log.info(f'build completed for {counter} in {date.month_name()} and start import to postgres')
+                    log.info(
+                        f'build completed for {counter} in {date.month_name()} and start import to postgres')
                     df.to_sql('cosmo', con=self.engine, if_exists='append')
-                    log.info(f'import in postgres complete --> start with next hour')
+                    log.info(
+                        'import in postgres complete --> start with next hour')
                     counter += 1                                            # counter ++
             except Exception:
                 log.exception(f'could not read {date}')
 
 
 if __name__ == '__main__':
-    crawler = OpenDWDCrawler()
-
+    import sqlite3
+    engine = sqlite3.connect('dwd.db')
+    crawler = OpenDWDCrawler(engine)
