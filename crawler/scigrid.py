c9567dc129ec866694b11fcd8e059db214751186
Christian Rieke <Rieke@fh-aachen.de>
2021-12-19 13:20:54 +0100

    rebuild scigrid with nuts

--- crawler/scigrid.py
+++ crawler/scigrid.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import requests, zipfile, io
+import geopandas as gpd
 import shapefile
+import shapely.wkt
 from shapely.geometry import shape, Point
 
 
 if __name__ == "__main__":
     from sqlalchemy import create_engine
     engine = create_engine('postgresql://opendata:opendata@10.13.10.41:5432/scigrid')
-    shape3_germany = shapefile.Reader(r'./shapes/plz-5stellig.shp')
-    plz_areas_3 = {feature.record['plz']: shape(feature.shape.__geo_interface__)
-                  for feature in shape3_germany.shapeRecords()}
 
     response = requests.get('https://www.power.scigrid.de/releases_archive/scigrid-conference-eu-data-only.zip')
     z = zipfile.ZipFile(io.BytesIO(response.content))
@@ -20,38 +19,55 @@ if __name__ == "__main__":
         if 'vertices_eu_power_160718.csvdata.xlsx' in name:
             nodes = pd.read_excel(z.open(name))
 
-    areas_1 = []
-    areas_2 = []
-    for index, row in links.iterrows():
-        line = eval(row['wkt_srid_4326'].split(';')[-1].replace('LINESTRING', '').replace(' ', ','))
-        coord_1 = Point(line[0], line[1])
-        coord_2 = Point(line[2], line[3])
-        area_1, area_2 = -1, -1
-        for key, area in plz_areas_3.items():
-            if area.contains(coord_1):
-                area_1 = int(key)
-                areas_1.append((index, area_1))
-            if area.contains(coord_2):
-                area_2 = int(key)
-                areas_2.append((index, area_2))
-            if area_1 > -1 and area_2 > -1:
+    new_columns = list(nodes.columns[:-1])
+    new_columns.append('geometry')
+    nodes.columns = new_columns
+
+    nodes['geometry'] = [shapely.wkt.loads(point.replace('SRID=4326;', '')) for point in nodes['geometry']]
+    nodes = gpd.GeoDataFrame(nodes)
+    # nodes.plot()
+
+    new_columns = list(links.columns[:-1])
+    new_columns.append('geometry')
+    links.columns = new_columns
+
+    links['geometry'] = [shapely.wkt.loads(line.replace('SRID=4326;', '')) for line in links['geometry']]
+    links = gpd.GeoDataFrame(links)
+    # links.plot()
+
+    eu_nuts = gpd.read_file(r'../shapes/NUTS_EU.shp')
+    eu_nuts = eu_nuts[eu_nuts['LEVL_CODE'] == 3]
+    eu_nuts = eu_nuts.to_crs(4326)
+
+    areas = []
+    for index, link in links.iterrows():
+        points = link['geometry']
+        points = points.coords.xy
+        point_1 = Point((points[0][0], points[1][0]))
+        point_2 = Point((points[0][1], points[1][1]))
+        a1 = None
+        a2 = None
+        for _, nut in eu_nuts.iterrows():
+            geom = nut['geometry']
+            if geom.contains(point_1):
+                a1 = nut['NUTS_ID']
+            if geom.contains(point_2):
+                a2 = nut['NUTS_ID']
+            if a1 is not None and a2 is not None:
                 break
-    for index, area in areas_1:
-        links.at[index, 'plz1'] = area
-    for index, area in areas_2:
-        links.at[index, 'plz2'] = area
+        areas.append((a1, a2))
+    links['areas'] = areas
+    links['geometry'] = links['geometry'].to_numpy(str)
     links.to_sql('edges', engine, if_exists='replace', index=False)
 
     areas = []
-    for index, row in nodes.iterrows():
-        point = eval(row['wkt_srid_4326'].split(';')[-1].replace('POINT', '').replace(' ', ','))
-        coord_1 = Point(point[0], point[1])
-        for key, area in plz_areas_3.items():
-            if area.contains(coord_1):
-                area = int(key)
-                areas.append((index,area))
+    for index, node in nodes.iterrows():
+        point = node['geometry']
+        for _, nut in eu_nuts.iterrows():
+            geom = nut['geometry']
+            if geom.contains(point):
+                areas.append(nut['NUTS_ID'])
                 break
-    for index, area in areas:
-        nodes.at[index, 'plz'] = area
-
+    nodes['area'] = areas
+    nodes['geometry'] = nodes['geometry'].to_numpy(str)
     nodes.to_sql('nodes', engine, if_exists='replace', index=False)
