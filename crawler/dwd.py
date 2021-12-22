d7f2045f37abfd8b5eb158dfbd2ac46126985b49
Christian Rieke <rieke@fh-aachen.de>
2021-12-22 10:15:15 +0100

    rebuild dwd crawler with nuts

--- crawler/dwd.py
+++ crawler/dwd.py
@@ -1,4 +1,3 @@
-import time
 import requests
 import bz2
 import pygrib
@@ -6,144 +5,106 @@ import pandas as pd
 import numpy as np
 import logging
 import os
-import sqlite3
 from tqdm import tqdm
+from sqlalchemy import create_engine
+
 
 log = logging.getLogger('openDWD_cosmo')
 log.setLevel(logging.INFO)
 
+nuts_matrix = np.load(r'./nuts_matrix.npy', allow_pickle=True)
+nuts = np.unique(nuts_matrix[[nuts_matrix != 'x']].reshape((-1)))
+countries = np.asarray([nut[:2] for nut in nuts])
+values = np.zeros_like(nuts)
 
-class OpenDWDCrawler:
+# own database
+engine = create_engine(f'postgresql://opendata:opendata@10.13.10.41:5432/weather')
+# open dwd data
+base_url = 'https://opendata.dwd.de/climate_environment/REA/COSMO_REA6/hourly/2D/'
+to_download = dict(temp_air='T_2M/T_2M.2D.',
+                   ghi='ASOB_S/ASOB_S.2D.',
+                   dni='ASWDIFD_S/ASWDIFD_S.2D.',
+                   dhi='ASWDIR_S/ASWDIR_S.2D.',
+                   wind_meridional='V_10M/V_10M.2D.',
+                   wind_zonal='U_10M/U_10M.2D.',
+                   rain_con='RAIN_CON/RAIN_CON.2D.',
+                   rain_gsp='RAIN_GSP/RAIN_GSP.2D.',
+                   cloud_cover='CLCT/CLCT.2D.')
 
-    def __init__(self, engine, folder='./grb_files', create_database=True):
-        log.info('initialize dwd cosmo crawler')
+def create_table():
+    engine.execute("CREATE TABLE IF NOT EXISTS public.cosmo( "
+                    "time timestamp without time zone NOT NULL, "
+                    "nut text, "
+                    "temp_air double precision, "
+                    "ghi double precision, "
+                    "dni double precision, "
+                    "dhi double precision, "
+                    "wind_meridional double precision, "
+                    "wind_zonal double precision, "
+                    "rain_con double precision,"
+                    "rain_gsp double precision, "
+                    "cloud_cover double precision, "
+                    "PRIMARY KEY (time , nut));")
 
-        # base url and parameters to get the weather data from open dwd cosmo model
-        self.base_url = 'https://opendata.dwd.de/climate_environment/REA/COSMO_REA6/hourly/2D/'
-        self.codecs = dict(temp_air='T_2M/T_2M.2D.',
-                           ghi='ASOB_S/ASOB_S.2D.',
-                           dni='ASWDIFD_S/ASWDIFD_S.2D.',
-                           dhi='ASWDIR_S/ASWDIR_S.2D.',
-                           wind_meridional='V_10M/V_10M.2D.',
-                           wind_zonal='U_10M/U_10M.2D.',
-                           rain_con='RAIN_CON/RAIN_CON.2D.',
-                           rain_gsp='RAIN_GSP/RAIN_GSP.2D.',
-                           cloud_cover='CLCT/CLCT.2D.')
+    query_create_hypertable = "SELECT create_hypertable('cosmo', 'time', if_not_exists => TRUE, migrate_data => TRUE);"
+    with engine.connect() as connection:
+        with connection.begin():
+            connection.execute(query_create_hypertable)
 
-        log.info(f'collect: {self.codecs.keys()}')
 
-        self.engine = engine
-        if create_database and type(self.engine) != sqlite3.Connection:
-            self.engine.execute("CREATE TABLE IF NOT EXISTS public.cosmo( "\
-                                "time timestamp without time zone NOT NULL, "\
-                                "nuts text, "\
-                                "temp_air double precision, "\
-                                "ghi double precision, " \
-                                "dni double precision, "\
-                                "dhi double precision, "\
-                                "wind_meridional double precision, "\
-                                "wind_zonal double precision, "\
-                                "rain_con double precision,"\
-                                "rain_gsp double precision, "\
-                                "cloud_cover double precision, "\
-                                "PRIMARY KEY (time , nuts));")
+def download_data(key, year, month):
+    response = requests.get(f'{base_url}{to_download[key]}{year}{month}.grb.bz2')
+    log.info(f'get weather for {key} with status code {response.status_code}')
 
-        query_create_hypertable = "SELECT create_hypertable('cosmo', 'time', if_not_exists => TRUE, migrate_data => TRUE);"
-        with self.engine.connect() as connection:
-            with connection.begin():
-                connection.execute(query_create_hypertable)
+    weather_data = bz2.decompress(response.content)
+    with open(f'./grb_files/weather{year}{month}', 'wb') as file:
+        file.write(weather_data)
 
-        self.weather_file_name = 'dwd.grb'
-        self.folder = folder
+def delete_data(year, month):
+    if os.path.isfile(f'./grb_files/weather{year}{month}'):
+        os.remove(f'./grb_files/weather{year}{month}')
 
-        if not os.path.exists(self.folder):
-            os.makedirs(self.folder)
+def create_dataframe(key, year, month):
+    weather_data = pygrib.open(f'./grb_files/weather{year}{month}')
+    selector = str(weather_data.readline()).split('1:')[1].split(':')[0]
+    size = len(weather_data.select(name=selector))
 
-        self.nuts_matrix = np.load(r'./nuts_matrix.npy', allow_pickle=True)
+    data_frames = []
+    for k in range(size):
+        data_ = weather_data.select(name=selector)[k]
+        df = pd.DataFrame(columns=[key],
+                          data={key: data_.values[nuts_matrix != 'x'].reshape((-1)),
+                                'nut': nuts_matrix[nuts_matrix != 'x'].reshape((-1))})
+        df = pd.DataFrame(df.groupby(['nut'])[key].mean())
+        data_frames.append(df)
 
-        log.info('crawler initialized')
+    log.info(f'read data with type: {key} in month {month}')
+    weather_data.close()
+    log.info('closed weather file')
 
-    def __del__(self):
-        for key in self.codecs.keys():
-            try:
-                file_name = f'{self.folder}/{key}_{self.weather_file_name}'
-                if os.path.isfile(file_name):
-                    os.remove(file_name)
-            except Exception:
-                log.error(f'error cleaning up file {file_name}')
-        self.engine.dispose()
+    return pd.concat(data_frames, ignore_index=True)
 
-    def save_data_in_file(self, typ='temp_air', year='1995', month='01'):
-        # get data of type for year and month
-        for i in range(1, 4):
-            try:
-                response = requests.get(f'{self.base_url}{self.codecs[typ]}{year}{month}.grb.bz2')
-                break
-            except Exception as e:
-                print(repr(e))
-                time.sleep(i**2)
 
-        log.info(f'get weather for {typ} with status code {response.status_code}')
-
-        # unzip an save data in file (parameter_weather.grb)
-        weather_data = bz2.decompress(response.content)
-        file_name = f'{self.folder}/{typ}_{self.weather_file_name}'
-        with open(file_name, 'wb') as file:
-            file.write(weather_data)
-
-        log.info(f'file {file_name} saved')
-
-    def read_data_file(self, date, typ):
-        # load dwd file with typ
-        file_name = f'{self.folder}/{typ}_{self.weather_file_name}'
-        weather_data = pygrib.open(file_name)
-        # extract the selector to get the correct parameter
-        selector = str(weather_data.readline()).split('1:')[1].split(':')[0]
-        # build dataframe and write data for each hour in month
-        hours = pd.date_range(start=pd.to_datetime(date), end=pd.to_datetime(date) + pd.DateOffset(months=1),
-                              freq='h')[:-1]
-        # slice the current hour with counter
-        size = len(weather_data.select(name=selector))
-        data_frames = []
-        for k in range(len(hours)):
-            data_ = weather_data.select(name=selector)[k]
-            df = pd.DataFrame()
-            # build dataframe
-            df[typ] = data_.values[self.nuts_matrix != 'x'].reshape((-1))
-            df['nuts'] = self.nuts_matrix[[self.nuts_matrix != 'x']].reshape((-1))
-            df = pd.DataFrame(df.groupby(['nuts'])[typ].mean())
-            df['nuts'] = df.index
-            df['country'] = [nut[:2] for nut in df['nut'].values]
-            df['time'] = hours[k]
-
-        log.info(f'read data with type: {typ} in month {date.month}')
-        weather_data.close()
-        log.info('closed weather file')
-
-        return pd.concat(data_frames)
-
-    def write_weather_in_timescale(self, start='199501', end='199502'):
-        date_range = pd.date_range(start=pd.to_datetime(start, format='%Y%m'),
-                                   end=pd.to_datetime(end, format='%Y%m'),
-                                   freq='MS')
-        for date in tqdm(date_range):
-            try:
-                # download files
-                for key in self.codecs.keys():
-                    self.save_data_in_file(year=str(date.year), month=f'{date.month:02d}', typ=key)
-                df = pd.DataFrame(columns=[key for key in self.codecs.keys()])
-                # create data frame for each parameter
-                for key in self.codecs.keys():
-                    df[key] = self.read_data_file(date, key)[key]
-                    log.info('build dataset for import')
-
-                index = pd.MultiIndex.from_arrays([df['time'], df['nuts']], names=['time', 'nuts'])
-                df.index = index
-                del df['time'], df['nuts']
-                log.info(f'built data for  {date.month_name()} and start import to postgres')
-                df.to_sql('cosmo_test', con=self.engine, if_exists='append')
-                log.info('import in postgres complete --> start with next hour')
-
-            except Exception as e:
-                print(repr(e))
-                log.exception(f'could not read {date}')
+def write_data(start, end):
+    date_range = pd.date_range(start=pd.to_datetime(start, format='%Y%m'),
+                               end=pd.to_datetime(end, format='%Y%m'),
+                               freq='MS')
+    for date in tqdm(date_range):
+        try:
+            df = pd.DataFrame(columns=[key for key in to_download.keys()])
+            for key in to_download.keys():
+                download_data(key, str(date.year), f'{date.month:02d}')
+                df[key] = create_dataframe(key, str(date.year), f'{date.month:02d}')
+                delete_data(str(date.year), f'{date.month:02d}')
+            df['time'] = pd.date_range(start=date, periods=len(df), freq='h')
+            df['country'] = countries
+            df['nut'] = nuts
+            index = pd.MultiIndex.from_arrays([df['time'], df['nut']], names=['time', 'nut'])
+            df.index = index
+            del df['time'], df['nuts']
+            log.info(f'built data for  {date.month_name()} and start import to postgres')
+            df.to_sql('cosmo_test', con=self.engine, if_exists='append')
+            log.info('import in postgres complete --> start with next hour')
+        except Exception as e:
+            print(repr(e))
+            log.exception(f'could not read {date}')
--- main_dwd.py
+++ main_dwd.py
@@ -2,7 +2,7 @@ import numpy as np
 import geopandas as gpd
 import pandas as pd
 
-from crawler.dwd import OpenDWDCrawler
+from crawler.dwd import write_data as download_data, create_table
 import logging
 import os
 from sqlalchemy import create_engine
@@ -11,18 +11,10 @@ from crawler.nut_mapper import create_nuts_map
 
 logging.basicConfig()
 
-user = os.getenv('TIMESCALEDB_USER', 'opendata')
-password = os.getenv('TIMESCALEDB_PASSWORD', 'opendata')
-database = os.getenv('TIMESCALEDB_DATABASE', 'weather')
-host = os.getenv('TIMESCALEDB_HOST', '10.13.10.41')
-port = int(os.getenv('TIMESCALEDB_PORT', 5432))
-
 
 def collect_data(start, end):
     try:
-        engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
-        crawler = OpenDWDCrawler(engine, folder=f'./grb_files{start}', create_database=False)
-        crawler.write_weather_in_timescale(start, end)
+        download_data(start, end)
     except Exception as e:
         print(repr(e))
         logging.exception(f'Error in worker with interval {start} - {end}')
@@ -30,18 +22,20 @@ def collect_data(start, end):
 
 if __name__ == "__main__":
 
-    max_processes = mp.cpu_count() - 1
-    dwd_latitude_range = np.load(r'./crawler/data/lat_coordinates.npy')
-    dwd_longitude_range = np.load(r'./crawler/data/lon_coordinates.npy')
+    create_nut_matrix = False
 
-    with mp.Pool(max_processes) as pool:
-        result = pool.map(create_nuts_map, [(i, j) for i in range(824) for j in range(848)])
+    if create_nut_matrix:
+        max_processes = mp.cpu_count() - 1
+        dwd_latitude_range = np.load(r'./crawler/data/lat_coordinates.npy')
+        dwd_longitude_range = np.load(r'./crawler/data/lon_coordinates.npy')
 
-    result = np.asarray(result).reshape((824, 848))
-    np.save('./nuts_matrix', result)
+        with mp.Pool(max_processes) as pool:
+            result = pool.map(create_nuts_map, [(i, j) for i in range(824) for j in range(848)])
 
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
-    crawler = OpenDWDCrawler(engine, create_database=True)
+        result = np.asarray(result).reshape((824, 848))
+        np.save('./nuts_matrix', result)
+
+    create_table()
 
     processes = []
     for year in range(1995, 2019):
@@ -51,4 +45,3 @@ if __name__ == "__main__":
 
     for process in processes:
         process.join()
-
