c79362ac482144865066cb5db6243661b8baccb0
Florian Maurer <maurer@fh-aachen.de>
2022-12-09 09:34:23 +0100

    fix select_latest query

--- crawler/eview.py
+++ crawler/eview.py
@@ -15,7 +15,7 @@ import re
 log = logging.getLogger('eview')
 log.setLevel(logging.INFO)
 
-default_start_date = date(2009,1,1)
+default_start_date = date(2022,11,1)
 # using http instead of https to be faster
 
 class EViewCrawler(BasicDbCrawler):
@@ -31,8 +31,12 @@ class EViewCrawler(BasicDbCrawler):
             day = datetime.strftime(fetch_date,'%d.%m.%Y')
             url = f"http://www.eview.de/solarstromdaten/export.php?p=;{unit};z;dg1;f0;t{day}/1;km250"
             data = requests.get(url)
-            df = pd.read_csv(url, decimal=',', index_col=0, encoding='iso-8859-1', skiprows=4, parse_dates=True)
-            log.info(f'num records {df.size}')
+            try:
+                df = pd.read_csv(url, decimal=',', index_col=0, encoding='iso-8859-1', skiprows=4, parse_dates=True)
+                log.info(f'num records {df.size}')
+            except Exception:
+                log.info('not data')
+                return
             if df.size < 2:
                 log.info('not data')
                 return
@@ -48,11 +52,14 @@ class EViewCrawler(BasicDbCrawler):
     def crawl_unit(self, unit, begin_date):
         first_date = pd.to_datetime(begin_date)+timedelta(days=1)
         last_date = date.today()-timedelta(days=1)
+        log.info(f'fetching from {first_date} until {last_date}')
         for fetch_date in pd.date_range(first_date, last_date):
             self.crawl_unit_date(unit, fetch_date)
 
     def select_latest(self,unit):
-        sql = f"select datetime from eview where plant_id='{unit}' order by datetime desc limit 1"
+        day = datetime.strftime(default_start_date,'%Y-%m-%d')
+        today = datetime.strftime(date.today(),'%Y-%m-%d')
+        sql = f"select datetime from eview where plant_id='{unit}' and datetime > '{day}' and datetime < '{today}' order by datetime desc limit 1"
         with self.db_accessor() as connection:
             try:
                 return pd.read_sql(sql,connection, parse_dates=['datetime']).values[0][0]
@@ -74,6 +81,7 @@ def main(db_uri):
     solar_plants = ec.get_solar_units()
 
     for plant in solar_plants:
+        log.info(f'fetching {plant}')
         try:
             begin_date = ec.select_latest(plant)
             ec.crawl_unit(plant,begin_date)
@@ -88,5 +96,8 @@ if __name__ == '__main__':
     db_uri = 'sqlite:///./data/eview.db'
     log.info(f'connect to {db_uri}')
     ec = EViewCrawler(db_uri)
+    plant = 'FI'
+    begin_date = ec.select_latest(plant)
+    ec.crawl_unit(plant, begin_date)
 
-    #main(db_uri)
+#    main(db_uri)
