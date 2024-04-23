8f35173831dc53ccd1c6688e55289844e91172d5
Marvin Lorber <lorber@fh-aachen.de>
2024-04-23 15:50:47 +0200

    feat(netztransparenz: add methods for more categories

--- crawler/netztransparenz.py
+++ crawler/netztransparenz.py
@@ -120,8 +120,8 @@ class NetztransparenzCrawler:
                 na_values=["N.A."]
                 )
             df.rename(mapper = lambda x: database_friendly(x), axis="columns" , inplace= True)
-            df["von"] = pd.to_datetime(df["datum"] + " " + df["von"] + " " + df["zeitzone_von"], format="%Y-%m-%d %H:%M %Z", utc=True)
-            df["bis"] = pd.to_datetime(df["datum"] + " " + df["bis"] + " " + df["zeitzone_bis"], format="%Y-%m-%d %H:%M %Z", utc=True)
+            df["von"] = pd.to_datetime(df["datum"] + " " + df["von"] + " " + df["zeitzone_von"], format=csv_date_format, utc=True)
+            df["bis"] = pd.to_datetime(df["datum"] + " " + df["bis"] + " " + df["zeitzone_bis"], format=csv_date_format, utc=True)
             df = df.drop(["datum", "zeitzone_von", "zeitzone_bis"], axis=1).set_index("von")
             with self.engine.begin() as conn:
                 df.to_sql(tablename, conn, if_exists="append")
@@ -143,8 +143,31 @@ class NetztransparenzCrawler:
                 na_values=["N.A."]
                 )
             df.rename(mapper = lambda x: database_friendly(x), axis="columns" , inplace= True)
-            df["von"] = pd.to_datetime(df["datum"] + " " + df["von"] + " " + df["zeitzone_von"], format="%Y-%m-%d %H:%M %Z", utc=True)
-            df["bis"] = pd.to_datetime(df["datum"] + " " + df["bis"] + " " + df["zeitzone_bis"], format="%Y-%m-%d %H:%M %Z", utc=True)
+            df["von"] = pd.to_datetime(df["datum"] + " " + df["von"] + " " + df["zeitzone_von"], format=csv_date_format, utc=True)
+            df["bis"] = pd.to_datetime(df["datum"] + " " + df["bis"] + " " + df["zeitzone_bis"], format=csv_date_format, utc=True)
+            df = df.drop(["datum", "zeitzone_von", "zeitzone_bis"], axis=1).set_index("von")
+            with self.engine.begin() as conn:
+                df.to_sql(tablename, conn, if_exists="append")
+
+    def utilization_balancing_energy(self):
+        tablename = "vermarktung_inanspruchnahme_ausgleichsenergie"
+        start_of_data = "2011-03-31T22:00:00"
+        start_of_data = self.find_latest(tablename, "bis", start_of_data)
+        end_of_data = dt.date.today() - dt.timedelta(days=1)
+        end_of_data = dt.datetime.combine(end_of_data, dt.datetime.min.time()).strftime(api_date_format)
+        if(start_of_data < end_of_data):
+            url = f"https://ds.netztransparenz.de/api/v1/data/vermarktung/InanspruchnahmeAusgleichsenergie/{start_of_data}/{end_of_data}"
+            response = requests.get(url, headers = {'Authorization': 'Bearer {}'.format(self.token)})
+            df = pd.read_csv(io.StringIO(response.text),
+                sep=";",
+                header=0,
+                decimal=",",
+                thousands=".",
+                na_values=["N.A."]
+                )
+            df.rename(mapper = lambda x: database_friendly(x), axis="columns" , inplace= True)
+            df["von"] = pd.to_datetime(df["datum"] + " " + df["von"] + " " + df["zeitzone_von"], format=csv_date_format, utc=True)
+            df["bis"] = pd.to_datetime(df["datum"] + " " + df["bis"] + " " + df["zeitzone_bis"], format=csv_date_format, utc=True)
             df = df.drop(["datum", "zeitzone_von", "zeitzone_bis"], axis=1).set_index("von")
             with self.engine.begin() as conn:
                 df.to_sql(tablename, conn, if_exists="append")
@@ -162,11 +185,146 @@ class NetztransparenzCrawler:
                 sep=";",
                 header=0,
                 decimal=",",
+                na_values=["N.A."]
                 )
             df.rename(mapper=str.lower, axis="columns", inplace=True)
             df["beginn"] = pd.to_datetime(df["beginn_datum"] + " " + df["beginn_uhrzeit"], format="%d.%m.%Y %H:%M", utc=True)
             df["ende"] = pd.to_datetime(df["ende_datum"] + " " + df["ende_uhrzeit"], format="%d.%m.%Y %H:%M", utc=True)
             df = df.drop(["beginn_datum", "beginn_uhrzeit", "ende_datum", "ende_uhrzeit", "zeitzone_von", "zeitzone_bis"], axis=1).set_index("beginn")
+            with self.engine.begin() as conn:
+                df.to_sql(tablename, conn, if_exists="append")
+
+    def gcc_balance(self):
+        tablename = "nrv_saldo"
+        start_of_data = "2014-01-01T00:00:00"
+        start_of_data = self.find_latest(tablename, "bis", start_of_data)
+        # Data might be subject to change for 20 Work days, so we wait 30 calendar days to crawl
+        end_of_data = dt.date.today() - dt.timedelta(days=30)
+        end_of_data = dt.datetime.combine(end_of_data, dt.datetime.min.time()).strftime(api_date_format)
+        if(start_of_data < end_of_data):
+            url = f"https://ds.netztransparenz.de/api/v1/data/nrvsaldo/NRVSaldo/Qualitaetsgesichert/{start_of_data}/{end_of_data}"
+            response = requests.get(url, headers = {'Authorization': 'Bearer {}'.format(self.token)})
+            df = pd.read_csv(io.StringIO(response.text),
+                sep=";",
+                header=0,
+                decimal=",",
+                na_values=["N.A."]
+                )
+            df.rename(mapper=str.lower, axis="columns", inplace=True)
+            df["von"] = pd.to_datetime(df["datum"] + " " + df["von"] + " " + df["zeitzone"], format="%d.%m.%Y %H:%M %Z", utc=True)
+            df["bis"] = pd.to_datetime(df["datum"] + " " + df["bis"] + " " + df["zeitzone"], format="%d.%m.%Y %H:%M %Z", utc=True)
+            df = df.drop(["datum", "zeitzone"], axis=1).set_index("von")
+            with self.engine.begin() as conn:
+                df.to_sql(tablename, conn, if_exists="append")
+
+    def lfc_area_balance(self):
+        tablename = "rz_saldo"
+        start_of_data = "2014-01-01T00:00:00"
+        start_of_data = self.find_latest(tablename, "bis", start_of_data)
+        # Data might be subject to change for 20 Work days, so we wait 30 calendar days to crawl
+        end_of_data = dt.date.today() - dt.timedelta(days=30)
+        end_of_data = dt.datetime.combine(end_of_data, dt.datetime.min.time()).strftime(api_date_format)
+        if(start_of_data < end_of_data):
+            url = f"https://ds.netztransparenz.de/api/v1/data/nrvsaldo/RZSaldo/Qualitaetsgesichert/{start_of_data}/{end_of_data}"
+            response = requests.get(url, headers = {'Authorization': 'Bearer {}'.format(self.token)})
+            df = pd.read_csv(io.StringIO(response.text),
+                sep=";",
+                header=0,
+                decimal=",",
+                na_values=["N.A."]
+                )
+            df.rename(mapper=str.lower, axis="columns", inplace=True)
+            df["von"] = pd.to_datetime(df["datum"] + " " + df["von"] + " " + df["zeitzone"], format="%d.%m.%Y %H:%M %Z", utc=True)
+            df["bis"] = pd.to_datetime(df["datum"] + " " + df["bis"] + " " + df["zeitzone"], format="%d.%m.%Y %H:%M %Z", utc=True)
+            df = df.drop(["datum", "zeitzone"], axis=1).set_index("von")
+            with self.engine.begin() as conn:
+                df.to_sql(tablename, conn, if_exists="append")
+    
+    def activated_automatic_balancing_capacity(self):
+        tablename = "aktivierte_srl"
+        start_of_data = "2013-01-01T00:00:00"
+        start_of_data = self.find_latest(tablename, "bis", start_of_data)
+        end_of_data = dt.date.today() - dt.timedelta(days=30)
+        end_of_data = dt.datetime.combine(end_of_data, dt.datetime.min.time()).strftime(api_date_format)
+        if(start_of_data < end_of_data):
+            url = f"https://ds.netztransparenz.de/api/v1/data/nrvsaldo/AktivierteSRL/Qualitaetsgesichert/{start_of_data}/{end_of_data}"
+            response = requests.get(url, headers = {'Authorization': 'Bearer {}'.format(self.token)})
+            df = pd.read_csv(io.StringIO(response.text),
+                sep=";",
+                header=0,
+                decimal=",",
+                na_values=["N.A."]
+                )
+            df.rename(mapper=str.lower, axis="columns", inplace=True)
+            df["von"] = pd.to_datetime(df["datum"] + " " + df["von"] + " " + df["zeitzone"], format="%d.%m.%Y %H:%M %Z", utc=True)
+            df["bis"] = pd.to_datetime(df["datum"] + " " + df["bis"] + " " + df["zeitzone"], format="%d.%m.%Y %H:%M %Z", utc=True)
+            df = df.drop(["datum", "zeitzone"], axis=1).set_index("von")
+            with self.engine.begin() as conn:
+                df.to_sql(tablename, conn, if_exists="append")
+
+    def activated_manual_balancing_capacity(self):
+        tablename = "aktivierte_mrl"
+        start_of_data = "2013-01-01T00:00:00"
+        start_of_data = self.find_latest(tablename, "bis", start_of_data)
+        end_of_data = dt.date.today() - dt.timedelta(days=30)
+        end_of_data = dt.datetime.combine(end_of_data, dt.datetime.min.time()).strftime(api_date_format)
+        if(start_of_data < end_of_data):
+            url = f"https://ds.netztransparenz.de/api/v1/data/nrvsaldo/AktivierteMRL/Qualitaetsgesichert/{start_of_data}/{end_of_data}"
+            response = requests.get(url, headers = {'Authorization': 'Bearer {}'.format(self.token)})
+            df = pd.read_csv(io.StringIO(response.text),
+                sep=";",
+                header=0,
+                decimal=",",
+                na_values=["N.A."]
+                )
+            df.rename(mapper=str.lower, axis="columns", inplace=True)
+            df["von"] = pd.to_datetime(df["datum"] + " " + df["von"] + " " + df["zeitzone"], format="%d.%m.%Y %H:%M %Z", utc=True)
+            df["bis"] = pd.to_datetime(df["datum"] + " " + df["bis"] + " " + df["zeitzone"], format="%d.%m.%Y %H:%M %Z", utc=True)
+            df = df.drop(["datum", "zeitzone"], axis=1).set_index("von")
+            with self.engine.begin() as conn:
+                df.to_sql(tablename, conn, if_exists="append")
+
+    def activated_manual_balancing_capacity(self):
+        tablename = "aktivierte_mrl"
+        start_of_data = "2013-01-01T00:00:00"
+        start_of_data = self.find_latest(tablename, "bis", start_of_data)
+        end_of_data = dt.date.today() - dt.timedelta(days=30)
+        end_of_data = dt.datetime.combine(end_of_data, dt.datetime.min.time()).strftime(api_date_format)
+        if(start_of_data < end_of_data):
+            url = f"https://ds.netztransparenz.de/api/v1/data/nrvsaldo/AktivierteMRL/Qualitaetsgesichert/{start_of_data}/{end_of_data}"
+            response = requests.get(url, headers = {'Authorization': 'Bearer {}'.format(self.token)})
+            df = pd.read_csv(io.StringIO(response.text),
+                sep=";",
+                header=0,
+                decimal=",",
+                na_values=["N.A."]
+                )
+            df.rename(mapper=str.lower, axis="columns", inplace=True)
+            df["von"] = pd.to_datetime(df["datum"] + " " + df["von"] + " " + df["zeitzone"], format="%d.%m.%Y %H:%M %Z", utc=True)
+            df["bis"] = pd.to_datetime(df["datum"] + " " + df["bis"] + " " + df["zeitzone"], format="%d.%m.%Y %H:%M %Z", utc=True)
+            df = df.drop(["datum", "zeitzone"], axis=1).set_index("von")
+            with self.engine.begin() as conn:
+                df.to_sql(tablename, conn, if_exists="append")
+
+    def value_of_avoided_activation(self):
+        tablename = "value_of_avoided_activation"
+        start_of_data = "2023-11-01T00:00:00"
+        start_of_data = self.find_latest(tablename, "bis", start_of_data)
+        end_of_data = dt.date.today() - dt.timedelta(days=30)
+        end_of_data = dt.datetime.combine(end_of_data, dt.datetime.min.time()).strftime(api_date_format)
+        if(start_of_data < end_of_data):
+            url = f"https://ds.netztransparenz.de/api/v1/data/nrvsaldo/VoAA/Qualitaetsgesichert/{start_of_data}/{end_of_data}"
+            response = requests.get(url, headers = {'Authorization': 'Bearer {}'.format(self.token)})
+            df = pd.read_csv(io.StringIO(response.text),
+                sep=";",
+                header=0,
+                decimal=",",
+                na_values=["N.A."]
+                )
+            df.rename(mapper=str.lower, axis="columns", inplace=True)
+            df["von"] = pd.to_datetime(df["datum"] + " " + df["von"] + " " + df["zeitzone"], format="%d.%m.%Y %H:%M %Z", utc=True)
+            df["bis"] = pd.to_datetime(df["datum"] + " " + df["bis"] + " " + df["zeitzone"], format="%d.%m.%Y %H:%M %Z", utc=True)
+            df = df.drop(["datum", "zeitzone"], axis=1).set_index("von")
             with self.engine.begin() as conn:
                 df.to_sql(tablename, conn, if_exists="append")
 
@@ -189,11 +347,17 @@ def database_friendly(string):
 def main():
     crawler = NetztransparenzCrawler(db_uri("netztransparenz"))
     #crawler.check_health()
-    crawler.forecast_solar()
+    #crawler.forecast_solar()
     #crawler.forecast_wind()
-    #crawler.extrapolation_solar()
-    #crawler.extrapolation_wind()
-    #crawler.redispatch()
+    crawler.extrapolation_solar()
+    crawler.extrapolation_wind()
+    crawler.utilization_balancing_energy()
+    crawler.redispatch()
+    crawler.gcc_balance()
+    crawler.lfc_area_balance()
+    crawler.activated_automatic_balancing_capacity()
+    crawler.activated_manual_balancing_capacity()
+    crawler.value_of_avoided_activation()
 
 
 if(__name__ == "__main__"):
