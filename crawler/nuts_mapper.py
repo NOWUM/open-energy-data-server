d472b79c480ea047266cce46ef1a0505ac01adba
Florian Maurer <maurer@fh-aachen.de>
2024-05-17 20:23:21 +0200

    add longitude and latitude centroids to nuts too

--- crawler/nuts_mapper.py
+++ crawler/nuts_mapper.py
@@ -37,6 +37,11 @@ def main(db_uri):
 
     # columns to lower
     geo_information.columns = map(str.lower, geo_information.columns)
+    # ignore warning, geographic CRS centroid are enough for us
+    # also see here: https://github.com/openclimatefix/nowcasting_dataset/issues/154#issuecomment-927148746
+    centroids = geo_information["geometry"].centroid
+    geo_information["longitude"] = centroids.x
+    geo_information["latitude"] = centroids.y
     geo_information.to_postgis("nuts", con=connection, if_exists="replace")
 
     # https://gisco-services.ec.europa.eu/tercet/flat-files
@@ -49,12 +54,6 @@ def main(db_uri):
         plz_list = pd.read_csv(f, sep=";", index_col="CODE", quotechar="'")
 
     # remove str literals from plzlist with read_csv
-
-    # ignore warning, geographic CRS centroid are enough for us
-    # also see here: https://github.com/openclimatefix/nowcasting_dataset/issues/154#issuecomment-927148746
-    centroids = geo_information["geometry"].centroid
-    geo_information["longitude"] = centroids.x
-    geo_information["latitude"] = centroids.y
     # where levl_code == 1 and country == DE
     geo_information = geo_information[geo_information["levl_code"] == 3]
     geo_information = geo_information[geo_information["cntr_code"] == "DE"]
