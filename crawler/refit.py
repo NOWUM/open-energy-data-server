79fbe4897a2f047b22e6aa42bb8022c40382e0d2
Florian Maurer <maurer@fh-aachen.de>
2024-05-31 18:52:20 +0200

    add refit data crawler

---
+++ crawler/refit.py
@@ -0,0 +1,51 @@
+import io
+import logging
+
+import pandas as pd
+import py7zr
+import requests
+from sqlalchemy import create_engine, text
+
+from .config import db_uri
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+REFIT_URL = (
+    "https://pure.strath.ac.uk/ws/portalfiles/portal/52873459/Processed_Data_CSV.7z"
+)
+
+
+def main(db_uri):
+    log.info("Download refit dataset")
+    response = requests.get(REFIT_URL)
+    log.info("Write refit to database")
+    engine = create_engine(db_uri)
+    with py7zr.SevenZipFile(io.BytesIO(response.content), mode="r") as z:
+        names = z.getnames()
+        # files = z.readall()
+        for name in names:
+            file = z.read([name])[name]
+            df = pd.read_csv(file, index_col="Time", parse_dates=["Time"])
+            del df["Unix"]
+            df["house"] = name
+            log.info(f"writing {name}")
+
+            with engine.begin() as conn:
+                df.to_sql("refit", conn, if_exists="append")
+    log.info("Finished writing REFIT to Database")
+
+    try:
+        query = text(
+            "select public.create_hypertable('refit.refit', 'Time', if_not_exists => TRUE, migrate_data => TRUE)"
+        )
+        with engine.begin() as conn:
+            conn.execute(query)
+        log.error("successfully created hypertable for REFIT")
+    except Exception:
+        log.error("could not create hypertable for REFIT")
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main(db_uri("refit"))
