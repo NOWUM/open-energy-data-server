d7a15fb0a8d179d362aa1d385b94ed05c14cdb81
vassily aliseyko <valiseyko@googlemail.com>
2024-07-03 13:11:38 +0200

    default connection string

--- crawler/config_example.py
+++ crawler/config_example.py
@@ -12,9 +12,13 @@ def db_uri_postgresql(db_name):
         + db_name
     )
 
-
 def db_uri_sqlite(db_name):
     return "sqlite:///" + db_name + ".db"
 
+def db_uri_default(db_name):
+    return (
+        "postgresql://opendata:opendata@localhost:6432/opendata?options=--search_path="
+        + db_name
+    )
 
-db_uri = db_uri_postgresql
+db_uri = db_uri_default
