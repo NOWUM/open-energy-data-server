6d61b5983d775be93f0bad08ed905532b86cc250
vassily aliseyko <valiseyko@googlemail.com>
2024-07-15 15:06:09 +0200

    update demo

---
+++ docs/minimal walkthrough/walkthrough_util.py
@@ -0,0 +1,53 @@
+# SPDX-FileCopyrightText: Vassily Aliseyko
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from sqlalchemy import text
+
+
+def db_uri_local_default(db_name):
+    return (
+        "postgresql://opendata:opendata@localhost:6432/opendata?options=--search_path="
+        + db_name
+    )
+
+db_uri = db_uri_local_default
+
+
+
+def create_schema_only(engine, schema_name):
+    with engine.begin() as conn:
+        conn.execute(
+            text(f"CREATE SCHEMA IF NOT EXISTS {schema_name}")
+        )
+        
+def set_metadata_only(engine, metadata_info):
+    with engine.begin() as conn:
+        conn.execute(
+            text("""
+            INSERT INTO public.metadata 
+            (schema_name, data_date, data_source, licence, description, contact, concave_hull_geometry, temporal_start, temporal_end)
+            VALUES 
+            (:schema_name, :data_date, :data_source, :licence, :description, :contact, :concave_hull_geometry, :temporal_start, :temporal_end)
+            ON CONFLICT (schema_name) DO UPDATE SET
+                data_date = EXCLUDED.data_date,
+                data_source = EXCLUDED.data_source,
+                licence = EXCLUDED.licence,
+                description = EXCLUDED.description,
+                contact = EXCLUDED.contact,
+                concave_hull_geometry = EXCLUDED.concave_hull_geometry,
+                temporal_start = EXCLUDED.temporal_start,
+                temporal_end = EXCLUDED.temporal_end
+            """),
+            metadata_info
+        )
+        conn.execute(
+            text("""
+            UPDATE public.metadata
+            SET tables = (SELECT COUNT(*) FROM pg_class JOIN pg_namespace ON pg_namespace.oid = pg_class.relnamespace WHERE nspname = :schema_name AND pg_class.relkind = 'r'),
+                size = (SELECT SUM(pg_total_relation_size(pg_class.oid)) FROM pg_class JOIN pg_namespace ON pg_namespace.oid = pg_class.relnamespace WHERE nspname = :schema_name AND pg_class.relkind = 'r'),
+                crawl_date = NOW()
+            WHERE schema_name = :schema_name
+            """),
+            {"schema_name": metadata_info["schema_name"]}
+        )
